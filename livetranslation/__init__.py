ae1473bb51b0ecdfb2a4db2d55f8ac59ed2809eb
Antti Kaihola <akaihol+django@ambitone.com>
2010-09-09 09:25:27 +0300

    Make all of django.utils.translation available in the livetranslation package

--- livetranslation/__init__.py
+++ livetranslation/__init__.py
@@ -1,12 +1,15 @@
 from django.utils.functional import lazy
-from django.utils.translation import (ugettext as django_ugettext,
-                                      ungettext as django_ungettext)
+from django.utils.translation import *
 from livetranslation.markup import mark_translation
 
+
+django_ugettext = ugettext
 
 def ugettext(message):
     return mark_translation(message, u'', django_ugettext(message))
 
+
+django_ungettext = ungettext
 
 def ungettext(singular, plural, number):
     return mark_translation(singular, plural,
